e229b797bd2f27a44a707b5b3af01ebbb7750340
Anas FANTES <111656976+afantes@users.noreply.github.com>
2023-05-04 12:51:56 +0100

    Add File

---
+++ Calc Textarea Chars Function/main.js
@@ -0,0 +1,18 @@
+let count = document.querySelector("#count"),
+    textarea = document.querySelector("#text"),
+    maxLength = textarea.getAttribute("maxlength");
+
+textarea.oninput = function () {
+
+    count.innerHTML = maxLength - this.value.length;
+
+    if (count.innerHTML == 0) {
+
+        count.classList.add("zero");
+
+    } else {
+
+        count.classList.remove("zero");
+
+    }
+};
